886724391a245db594404c2c0859908d4b9a80cc
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-07-09 16:56:56 +0900

    searchBar Design

--- newlyCoinedWord/MainViewController.swift
+++ newlyCoinedWord/MainViewController.swift
@@ -10,15 +10,43 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        searchBarDesign()
+        buttonDesign(button1)
+        buttonDesign(button2)
+        buttonDesign(button3)
+        buttonDesign(button4)
     }
     
     // 텍스트필드 디자인
+    func searchBarDesign() {
+        // 검색창 디자인
+        searchTextField.layer.borderWidth = 2
+        searchTextField.layer.borderColor = UIColor.black.cgColor
+        searchTextField.attributedPlaceholder = NSAttributedString(string: "검색어를 입력하세요", attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray])
+        
+        // 검색버튼 디자인
+        searchButton.layer.borderWidth = 2
+        searchButton.layer.borderColor = UIColor.black.cgColor
+        searchButton.backgroundColor = .black
+    }
     
+    // 검색기록 버튼 디자인
+    func buttonDesign(_ buttonName: UIButton) {
+        buttonName.layer.cornerRadius = 3
+        buttonName.layer.borderWidth = 1
+        buttonName.backgroundColor = .white
+        buttonName.setTitleColor(.black, for: .normal)
+    }
 
 
 }
