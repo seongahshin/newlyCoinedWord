5b035049a22c642010f081f66483ae494bcfae59
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-07-09 16:27:46 +0900

    newlyCoinedWord 오토레이아웃 설정

---
+++ newlyCoinedWord/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  newlyCoinedWord
+//
+//  Created by 신승아 on 2022/07/09.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    // 텍스트필드 디자인
+    
+
+
+}
