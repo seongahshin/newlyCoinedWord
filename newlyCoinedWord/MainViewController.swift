39107558c0ef4f4bc9a5dd1259042c5682d9c14f
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-07-09 17:45:08 +0900

    검색하는 단어와 뜻 연결

--- newlyCoinedWord/MainViewController.swift
+++ newlyCoinedWord/MainViewController.swift
@@ -17,6 +17,7 @@ class MainViewController: UIViewController {
     @IBOutlet weak var button3: UIButton!
     @IBOutlet weak var button4: UIButton!
     
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,7 +28,7 @@ class MainViewController: UIViewController {
         buttonDesign(button4)
     }
     
-    // 텍스트필드 디자인
+    // 서치바 디자인
     func searchBarDesign() {
         // 검색창 디자인
         searchTextField.layer.borderWidth = 2
@@ -53,13 +54,28 @@ class MainViewController: UIViewController {
         view.endEditing(true)
     }
     
+    // 검색 단어와 뜻 연결
+    func searchWordMeaning() {
+        if searchTextField.text == "점메추" {
+            resultLabel.text = "점심 메뉴 추천의 줄임말"
+        } else if searchTextField.text == "억텐" {
+            resultLabel.text = "억지 텐션의 줄임말. 반대말로 진짜 텐션은 찐텐이라고 한다."
+        } else if searchTextField.text == "보배" {
+            resultLabel.text = "보조배터리"
+        } else if searchTextField.text == "구취" {
+            resultLabel.text = "구독 취소의 줄임말. 유튜브 채널을 구독 취소할 때 많이 쓰인다."
+        }
+    }
+    
     // 검색 버튼 누르면 키보드 내리기
     @IBAction func buttonClicked(_ sender: UIButton) {
         view.endEditing(true)
+        searchWordMeaning()
     }
     
     // retun 버튼 누르면 키보드 내리기
     @IBAction func returnButtonClicked(_ sender: Any) {
+        searchWordMeaning()
     }
     
 }
